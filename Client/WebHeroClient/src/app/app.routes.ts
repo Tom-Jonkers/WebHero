425dc8c4894baf4ff7afea4d390d81c0a7182653
Tom-Jonkers <tomjonkers@hotmail.com>
2025-09-30 17:52:41 -0400

    FCT UI de base client

--- Client/WebHeroClient/src/app/app.routes.ts
+++ Client/WebHeroClient/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { BrowseComponent } from './browse/browse.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'browse', component: BrowseComponent },
+  { path: '', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
